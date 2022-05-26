e9a4ee186455305e861711b39ddc4ebc14bd9cdb
Usman Akram <uakram86@gmail.com>
2022-05-26 18:04:11 +0100

    completed bookmarks and listscontroller

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,38 @@
+class BookmarksController < ApplicationController
+  before_action :find_list, only: %i[new create]
+
+  # def index
+  #   @bookmarks = Bookmark.where(list_id: @list)
+  # end
+
+  def new
+    @bookmark = Bookmark.new
+    # @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
