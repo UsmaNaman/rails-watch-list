c64a8eb278e7e8d045e54d230a385b9740d2e8e1
Usman Akram <uakram86@gmail.com>
2022-08-26 10:51:34 +0100

    playing around and refactoring

---
+++ app/javascript/controllers/typed_js_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
+
+// Connects to data-controller="typed-js"
+export default class extends Controller {
+  connect() {
+    console.log("connected")
+    new Typed(this.element, {
+      strings: ["Create lists", "Watch movies"],
+      typeSpeed: 25,
+      loop: true
+    })
+  }
+}
