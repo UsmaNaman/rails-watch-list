54b4ba812d891a339ff596acd5c6600cd60a5605
Usman Akram <uakram86@gmail.com>
2023-02-27 12:15:53 +0000

    removed config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,5 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.assets.check_precompiled_asset = false
+  
 end
